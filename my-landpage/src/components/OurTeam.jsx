073487ebd3a265fdfe28d063ec918852fd3eac3a
Lucas C. Bresolino <lucascaladob@gmail.com>
2024-11-29 07:29:11 -0300

    feat: our team

--- my-landpage/src/components/OurTeam.jsx
+++ my-landpage/src/components/OurTeam.jsx
@@ -1,144 +1,145 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-export default function Button({theTextInside, href}) {
+import BgImage from "../images/BgTeam.jpg"
+import Mee from "../images/MyPhoto.jpg"
 
-    .steam{
-        background-image: linear-gradient(rgba(30,30,30,0.5),rgba(30,30,30,0.7)), url('../img/BgTeam.jpg');
+export default function OurTeam() {
+
+    const DefaultContainer = styled.section`
+        width: 100%;
+        padding: 50px 0;
+        background-image: linear-gradient(rgba(30,30,30,0.5),rgba(30,30,30,0.7)), url('${BgImage}');
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
-        height: 100vh;
-    }
-    .team{
+        color: #fff;
+  `;
+    const Title = styled.div`
+        text-align: center;
+        text-transform: capitalize;
+        font-size: 2.4rem;
+        font-weight: 600;
+  `;
+    const Description = styled.div`
+        text-align: center;
+        text-transform: capitalize;
+        font-size: 1.7rem;
+        font-weight: 600;
+        color: #a0a0a0;
+        margin-top: 1.3rem;
+        margin-bottom: 4.5rem;
+  `;
+    const Body = styled.div`
+        padding: 4rem;
+        margin: auto;
+  `;
+
+    const Team = styled.section`
         overflow: hidden;
         max-width: 890px;
         margin: auto;
-    }
-    .team .sliders{
+    `
+    const Sliders = styled.div`
         width: 400vw;
         height: 100%;
         transition: 2s;
-    }
-    .team .slide{
+    `
+    const Slide = styled.div`
         width: 280px;
         height: 340px;
-        display: inline-block;  
+        display: inline-block;
         margin-right: 2rem;
-    }
-    .team .slide-area{
+    `
+    const SlideArea = styled.div`
         height: 100%;
         display: flex;
         align-items: center;
         justify-content: center;
         flex-direction: column;
         color: #fff;
-        background-color: rgba(30,30,30,0.7);
+        background-color: rgba(30, 30, 30, 0.7);
         border: 1px groove var(--primary);
-    }
-    .team .sliders-pointer{
-        margin-top: 5rem;
-        height: 1.5rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .team .pointer{
-        width: 1.5rem;
-        height: 1.5rem;
-        border: 2px solid var(--primary);
-        border-radius: 8px;
-        margin: 0 4px;
-        cursor: pointer;
-    }
-    .team .pointer.active{
-        background-color: var(--primary);
-    }
-    .team-img{
+    `
+    const TeamImg = styled.img `
         width: 180px;
         height: 180px;
-        border: 1px groove rgba(30,30,30,1);
+        border: 1px groove rgba(30, 30, 30, 1);
         border-radius: 50%;
         object-fit: cover;
-    }
-    .team-name{
+    `
+    const TeamName = styled.h3 `
         text-transform: capitalize;
-        font-weight: 600;
+        font-weight: 300;
         font-size: 1.7rem;
         margin-top: 1.5rem;
         color: var(--primary);
-    }
-    .team-role{
+    `
+    const TeamRole = styled.p`
         font-size: 1.4rem;
         margin-top: 15px;
         color: #ccc;
-    }
-    .team-links .fab,.team-links .fas{
+    `
+    const LinkHub = styled.i`
         color: var(--primary);
         text-decoration: none;
         font-size: 1.7rem;
         margin: 20px 10px;
         background-color: transparent;
-    }
-    .team-links .fab:hover,.team-links .fas:hover{
-        transform: scale(1.3);
-    }
-    
+        &:hover{
+            transform: scale(1.3);
+        }
     `
+
     return (
-        <section class="default dark steam">
-            <div class="section-title">Our Team</div>
-            <div class="section-desc">Our Co-workers</div>
-            <div class="section-body">
-                <div class="team">
-                    <div class="sliders">
-                        <div class="slide">
-                            <div class="slide-area">
-                               <img src="media/Team-1.jpg" class="team-img"/>
-                               <h3 class="team-name">Lucas C. Bresolino</h3>
-                               <p class="team-role">Web and Mobile Fullstack </p>
+        <DefaultContainer id="ourteam">
+            <Title class="section-title">Our Team</Title>
+            <Description class="section-desc">Our Co-workers</Description>
+            <Body class="section-body">
+                <Team class="team">
+                    <Sliders class="sliders">
+                        <Slide class="slide">
+                            <SlideArea class="slide-area">
+                               <TeamImg src={Mee} class="team-img"/>
+                               <TeamName class="team-name">ERP and Chatbot Developer</TeamName>
+                               <TeamRole class="team-role">Lucas C. Bresolino</TeamRole>
                                <div class="team-links">
-                                   <a href=""><i class="fab fa-linkedin-in"></i></a>
-                                   <a href=""><i class="fab fa-instagram"></i></a>
-                                   <a href=""><i class="fab fa-discord"></i></a>
-                                   <a href="mailto:lucascaladob@gmail.com"><i class="fas fa-envelope"></i></a>
+                                   <a href=""><LinkHub class="fab fa-linkedin-in"></LinkHub></a>
+                                   <a href=""><LinkHub class="fab fa-instagram"></LinkHub></a>
+                                   <a href=""><LinkHub class="fab fa-discord"></LinkHub></a>
+                                   <a href="mailto:lucascaladob@gmail.com"><LinkHub class="fas fa-envelope"></LinkHub></a>
                                </div>
-                            </div>
-                        </div>
-                        <div class="slide">
-                            <div class="slide-area">
-                                <img src="media/Team-2.jpg" class="team-img"/>
-                                <h3 class="team-name">Lucas C. Bresolino</h3>
-                                <p class="team-role">Data Engineering</p>
+                            </SlideArea>
+                        </Slide>
+                        <Slide class="slide">
+                            <SlideArea class="slide-area">
+                                <TeamImg src={Mee} class="team-img"/>
+                                <TeamName class="team-name">Backend Engineering</TeamName>
+                                <TeamRole class="team-role">Lucas C. Bresolino</TeamRole>
                                 <div class="team-links">
-                                    <a href=""><i class="fab fa-linkedin-in"></i></a>
-                                    <a href=""><i class="fab fa-instagram"></i></a>
-                                    <a href=""><i class="fab fa-discord"></i></a>
-                                    <a href="mailto:lucascaladob@gmail.com"><i class="fas fa-envelope"></i></a>
+                                    <a href=""><LinkHub class="fab fa-linkedin-in"></LinkHub></a>
+                                    <a href=""><LinkHub class="fab fa-instagram"></LinkHub></a>
+                                    <a href=""><LinkHub class="fab fa-discord"></LinkHub></a>
+                                    <a href="mailto:lucascaladob@gmail.com"><LinkHub class="fas fa-envelope"></LinkHub></a>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="slide">
-                            <div class="slide-area">
-                                <img src="media/Team-3.jpg" class="team-img"/>
-                                <h3 class="team-name">Lucas C. Bresolino</h3>
-                                <p class="team-role">Cloud Master</p>
+                            </SlideArea>
+                        </Slide>
+                        <Slide class="slide">
+                            <SlideArea class="slide-area">
+                                <TeamImg src={Mee} class="team-img"/>
+                                <TeamName class="team-name">Cloud and Database Developer</TeamName>
+                                <TeamRole class="team-role">Lucas C. Bresolino</TeamRole>
                                 <div class="team-links">
-                                    <a href=""><i class="fab fa-linkedin-in"></i></a>
-                                    <a href=""><i class="fab fa-instagram"></i></a>
-                                    <a href=""><i class="fab fa-discord"></i></a>
-                                    <a href="mailto:lucascaladob@gmail.com"><i class="fas fa-envelope"></i></a>
+                                    <a href=""><LinkHub class="fab fa-linkedin-in"></LinkHub></a>
+                                    <a href=""><LinkHub class="fab fa-instagram"></LinkHub></a>
+                                    <a href=""><LinkHub class="fab fa-discord"></LinkHub></a>
+                                    <a href="mailto:lucascaladob@gmail.com"><LinkHub class="fas fa-envelope"></LinkHub></a>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sliders-pointer">
-                        <div class="pointer" onclick="clickBanner(1)"></div>
-                        <div class="pointer" onclick="clickBanner(2)"></div>
-                        <div class="pointer" onclick="clickBanner(3)"></div>
-                    </div>
-                </div>
-            </div>
-        </section>
+                            </SlideArea>
+                        </Slide>
+                    </Sliders>
+                </Team>
+            </Body>
+            </DefaultContainer>
     )
 }
